a872a7aa56d3adfc377e3eef18faf5bd2101afab
hwhwang2 <hwhwang2@illinois.edu>
2017-04-04 12:17:45 -0500

    run all

--- hw5/hw5_code/prob1i.py
+++ hw5/hw5_code/prob1i.py
@@ -2,9 +2,9 @@ import NN, data_loader, perceptron, random
 import multiprocessing as mp
 import parameter_tuning_paral as tune
 
-# training_data, test_data = data_loader.load_mnist_data()
-# domain = 'mnist'
-# tune.paraSweep (training_data,domain)
+training_data, test_data = data_loader.load_mnist_data()
+domain = 'mnist'
+tune.paraSweep (training_data,domain)
 training_data, test_data = data_loader.load_circle_data()
 domain = 'circles'
 tune.paraSweep (training_data,domain)
